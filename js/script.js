a0243c3e5413d023a3d8e4ba313582c7521486fd
xAbdx <67425264+xAbdx@users.noreply.github.com>
2020-12-17 23:37:01 +0200

    fixing the functions and add reset button

--- js/script.js
+++ js/script.js
@@ -1,10 +1,17 @@
 function changeMainColor() {
     let mainColor = document.getElementById('MainColor').value;
-    // alert('aasds');
-    document.getElementById('main').style.background = mainColor;
+    document.getElementById('main').style.setProperty('--main-color', mainColor);
 }
 
 function changeSubColor() {
     let subColor = document.getElementById('subColor').value;
-    document.getElementById('main').style.backgroundColor = subColor;
+    document.getElementById('main').style.setProperty('--sub-main', subColor);
+}
+
+let root = document.querySelector('.reset');
+root.addEventListener('click', reset);
+
+function reset() {
+    document.getElementById('main').style.removeProperty('--main-color');
+    document.getElementById('main').style.removeProperty('--sub-main');
 }
